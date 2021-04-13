20cdcfe8850f5c5fe2e50dbab73a28af3c5ce431
STM <stefa0826.14@gmail.com>
2021-04-12 20:20:08 -0500

    Pedido

--- belle_femme/src/main/java/com/udem/bellefemme/modelo/Pedido.java
+++ belle_femme/src/main/java/com/udem/bellefemme/modelo/Pedido.java
@@ -5,7 +5,13 @@ import javax.xml.bind.annotation.XmlRootElement;
 @XmlRootElement(name="Pedido")
 public class Pedido {
 	
-	
-	
+	private long codigoPedido;
+	private String nombre;
+	private String descripcion;
+	private String articulos;
+	private long cantidad;
+	private long precio;
+	private String cliente;
+	private String vendedor;
 	
 }
